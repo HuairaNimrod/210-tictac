8c5df85ee245b7b7c7e027e4decf5f62b76b0cad
nimax <nimax29@gmail.com>
2022-11-02 10:32:03 -0600

    board

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace cse210_tictac
+{
+    class Program{
+    static void Main(string[] args)
+    {
+
+        List<string> board = GetNewBoard();
+        
+        DisplayBoard(board);
+
+
+        
+    }
+    static List<string> GetNewBoard()
+    {
+
+            List<string> board = new List<string>();
+
+                for(int i=1; i<=9; i++)
+                {
+                    board.Add(i.ToString());
+                }
+
+        return board;
+    }
+
+    static void DisplayBoard(List<string> board)
+    {
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
+
+    }
+
+    }    
+    
+}
+
+// class Car 
+// {
+//   string color = "red";
+
+//   static void Main(string[] args)
+//   {
+//     Car myObj = new Car();
+//     Console.WriteLine(myObj.color);
+//   }
+// }
